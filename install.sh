88ac296643583acfc66b7bf203f77c7df9bae3a1
liam.liao <liam.liao@latticeworkinc.com>
2017-05-20 13:30:51 +0800

    [install.sh] install without something

--- install.sh
+++ install.sh
@@ -34,42 +34,42 @@ ln -sf `pwd`/bin ~/bin
 rm -rf ~/util_scripts
 ln -sf `pwd`/util_scripts ~/util_scripts
 
-uname -a | grep -i "ubuntu" > /dev/null
-if [ $? -eq 0 ]
-then
-    sudo apt-get update
-    sudo apt-get install -y -q python-pip python-dev build-essential 
-    sudo pip install --upgrade pip 
-    sudo pip install --upgrade virtualenv 
-    sudo pip install --upgrade pycscope
-    sudo apt-get install -y -q vim
-    sudo apt-get install -y -q git
-    sudo apt-get install -y -q tig
-    sudo apt-get install -y -q exuberant-ctags
-    sudo apt-get install -y -q cscope
-    sudo apt-get install -y -q cmake
-    sudo apt-get install -y -q g++
-    sudo apt-get install -y -q sqlite3
-    sudo apt-get install -y -q libsqlite3-dev
-    sudo apt-get install -y -q qt4-dev-tools
-    sudo apt-get install -y -q libqscintilla2-dev
-else
-    echo "other linux distrubution"
-fi
+# uname -a | grep -i "ubuntu" > /dev/null
+# if [ $? -eq 0 ]
+# then
+#     sudo apt-get update
+#     sudo apt-get install -y -q python-pip python-dev build-essential 
+#     sudo pip install --upgrade pip 
+#     sudo pip install --upgrade virtualenv 
+#     sudo pip install --upgrade pycscope
+#     sudo apt-get install -y -q vim
+#     sudo apt-get install -y -q git
+#     sudo apt-get install -y -q tig
+#     sudo apt-get install -y -q exuberant-ctags
+#     sudo apt-get install -y -q cscope
+#     sudo apt-get install -y -q cmake
+#     sudo apt-get install -y -q g++
+#     sudo apt-get install -y -q sqlite3
+#     sudo apt-get install -y -q libsqlite3-dev
+#     sudo apt-get install -y -q qt4-dev-tools
+#     sudo apt-get install -y -q libqscintilla2-dev
+# else
+#     echo "other linux distrubution"
+# fi
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#build codequery
-temp_dir=$(mktemp -d)
-cd $temp_dir
-git clone https://github.com/ruben2020/codequery.git
-cd codequery
-mkdir build
-cd build
-cmake -G "Unix Makefiles" ..
-make
-sudo make install
+# #build codequery
+# temp_dir=$(mktemp -d)
+# cd $temp_dir
+# git clone https://github.com/ruben2020/codequery.git
+# cd codequery
+# mkdir build
+# cd build
+# cmake -G "Unix Makefiles" ..
+# make
+# sudo make install
 
 vim +PluginInstall +qall
 vim +PlugInstall +qall
@@ -78,7 +78,7 @@ cd ~/.vim/bundle/YouCompleteMe
 ############################
 #  install python packages #
 ############################
-sudo pip install flake8
+# sudo pip install flake8
 
 cd ~
 
